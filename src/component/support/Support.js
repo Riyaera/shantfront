a52c46255843a025e5840d386feb12456898041b
Riya bisht <bisht.r2102@gmail.com>
2024-06-12 14:28:18 +0530

    glowing button made

--- src/component/support/Support.js
+++ src/component/support/Support.js
@@ -8,18 +8,17 @@ function Support() {
   return <div>
     <div className="wrapper clearfix">
     <section className="under-development-section">
-      <div className="nav-wrap">
-        <nav className="main-nav" role="navigation">
+      <div className="development">
+      
           <ul className="unstyled list-hover-slide">
-            <li><a href="/" >Software Downloads</a></li>
-            <li><a href="/" >Manuals</a></li>
-            <li><a href="/" >Cable Diagram</a></li>
-            <li><a href="/" >Tutorials</a></li>
+            <li className="hover-btn">Software Downloads</li>
+            <li className="hover-btn">Manuals</li>
+            <li className="hover-btn">Cable Diagram</li>
+            <li className="hover-btn">Tutorials</li>
+            
+
           </ul>
-        </nav>
-      </div>
-      <div className="content-wrap">
-        <h1>Page is under development</h1>
+
       </div>
     </section>
     </div>
